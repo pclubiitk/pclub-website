cb8dab1aaab868cf994fc8cb6586addc7178b070
Harshit Tomar <harshitt345@gmail.com>
2024-06-03 10:42:29 +0530

    fixed typo (numer instead of number)

--- updating-scripts/secy-csv-to-yaml/secy-csv-to-yaml.py
+++ updating-scripts/secy-csv-to-yaml/secy-csv-to-yaml.py
@@ -22,7 +22,7 @@ for secy in secys:
 	print(f"\t'{secy[0]}':")
 	print(f"\t\thall: {secy[1]}")
 	print(f"\t\tmail: {secy[2]}")
-	print(f"\t\tnumer: {secy[3]}")
+	print(f"\t\tnumber: {secy[3]}")
 	print(f"\t\tinsta_id: {secy[5]}")
 	print(f"\t\tgithub: {secy[4]}\n")
 
@@ -30,7 +30,7 @@ for secy in secys:
 		f"\t'{secy[0]}':\n",
 		f"\t\thall: {secy[1]}\n",
 		f"\t\tmail: {secy[2]}\n",
-		f"\t\tnumer: {secy[3]}\n",
+		f"\t\tnumber: {secy[3]}\n",
 		f"\t\tinsta_id: {secy[5]}\n",
 		f"\t\tgithub: {secy[4]}\n\n",
 	])
